43529c79d78686c256c2b63b92073b6882fbe206
gitbulk <giacatiensinh@hotmail.com>
2016-08-19 00:59:02 +0700

    Add system information for whole project

---
+++ RentalVideo.Base/SystemInformation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalVideo.Base
+{
+    public class SystemInformation
+    {
+        public static DateTime GetDate()
+        {
+            string result = ConfigurationManager.AppSettings["SysDateType"] as string;
+            if (!string.IsNullOrEmpty(result) && result.Equals("UtcType"))
+            {
+                return DateTime.UtcNow;
+            }
+
+            return DateTime.Now;
+        }
+    }
+}
